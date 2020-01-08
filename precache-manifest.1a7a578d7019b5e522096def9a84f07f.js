d50f1bca95ebb1474b82cb6301d807e0e4906214
funfish22 <j26391442@gmail.com>
2020-01-08 14:25:58 +0800

    first commit 開始使用json-server

---
+++ precache-manifest.1a7a578d7019b5e522096def9a84f07f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6896780b0861949c754f0db8cecb483",
+    "url": "./index.html"
+  },
+  {
+    "revision": "44f6ab5d09076c5d7b6f",
+    "url": "./static/css/main.f51c86aa.chunk.css"
+  },
+  {
+    "revision": "c20843e6d289dbdb1412",
+    "url": "./static/js/2.60c98c40.chunk.js"
+  },
+  {
+    "revision": "99d8f7ab4018b7871a948efd496aaea5",
+    "url": "./static/js/2.60c98c40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44f6ab5d09076c5d7b6f",
+    "url": "./static/js/main.ef98ae22.chunk.js"
+  },
+  {
+    "revision": "53e5936090e752ff4a4e",
+    "url": "./static/js/runtime-main.cea9507d.js"
+  },
+  {
+    "revision": "18c51a4242542f16eb577507b54f2156",
+    "url": "./static/media/icomoon.18c51a42.eot"
+  },
+  {
+    "revision": "3da50f08ea547959b207f6437bfff383",
+    "url": "./static/media/icomoon.3da50f08.ttf"
+  },
+  {
+    "revision": "565ca99d8b035979f70b6665ca89c3e1",
+    "url": "./static/media/icomoon.565ca99d.svg"
+  },
+  {
+    "revision": "cb67c7699f7374322c46ca89b449973e",
+    "url": "./static/media/icomoon.cb67c769.woff"
+  }
+]);
