47fbfe0d5e676e68d5f1d269edfa51d71e1b9663
funfish22 <j26391442@gmail.com>
2020-01-10 13:19:24 +0800

    新增 router 雙擊資料夾進入內頁

--- src/pages/App/Router.tsx
+++ src/pages/App/Router.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import Home from 'pages/default/Home';
+import Folder from 'pages/default/Folder';
 
 
 function Router() {
     return(
         <Switch>
             <Route path="/" exact component={Home} />
+            <Route path="/Folder/:name" component={Folder} />
 
             {/* <Route component={PageNotFound} /> */}
         </Switch>
